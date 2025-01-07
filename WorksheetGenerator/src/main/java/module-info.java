f0f68bec66ef35c6726b00a03a0170f70df3ea6a
asyylz <49663592+asyylz@users.noreply.github.com>
2025-01-07 21:01:32 +0000

    Delete WorksheetGenerator directory

--- WorksheetGenerator/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.wgapp.worksheetgenerator.worksheetgenerator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.wgapp.worksheetgenerator to javafx.fxml;
-    exports com.wgapp.worksheetgenerator;
-}
