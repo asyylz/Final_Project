272485d5f67b222a8cb835e410153688a14154c4
asyylz <49663592+asyylz@users.noreply.github.com>
2025-01-07 21:00:21 +0000

    Delete HandlingEvent directory

--- HandlingEvent/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.asiye.eventhandler {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.asiye.eventhandler to javafx.fxml;
-    exports com.asiye.eventhandler;
-}
